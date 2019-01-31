d9f891f62cefb6330c7018bce8b4db8385c58601
Mark Quinn <mark.quinn@infinityworks.com>
2019-01-31 09:24:11 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Startup:
+
+cd docker
+docker-compose up -d
+
+cd ..
+dotnet restore
+dotnet run
---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using elastic_kibana.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace elastic_kibana.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation($"oh hai there! : {DateTime.UtcNow}");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            try
+            {
+                throw new Exception("oops. i haz cause error in UR codez.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "ur code iz buggy.");
+            }
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
